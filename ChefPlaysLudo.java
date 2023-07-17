808bfdbc82bf38ed90aa5ff68a372347f6b156e8
A Dipner Reddy <89031602+dipnerreddy@users.noreply.github.com>
2023-07-17 10:53:23 +0530

    Add files via upload

---
+++ ChefPlaysLudo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ChefPlaysLudo {
+    public static void main(String[] args) {
+        	Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		for(int i=1;i<=T;i++){
+		    int x=sc.nextInt();
+		    if(x==6){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+    }
+}
